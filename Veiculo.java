487c358b639d32ceb3a9cd5ea25285be865db320
Viniciusslara <vinicius.lara2017@hotmail.com>
2023-12-16 22:14:44 -0300

    add: implementation to interface method

--- Veiculo.java
+++ Veiculo.java
@@ -2,5 +2,9 @@ package First_POO_Project;
 
 public class Veiculo extends Caracteristicas implements CalcConsumo{
 
-    
+
+    @Override
+    public float consumo(float media, int kilometragem){
+        return media * kilometragem;
+    }
 }
