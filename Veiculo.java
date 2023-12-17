d8e7011b198d90abb2cee590e53b066da8976d3b
Viniciusslara <vinicius.lara2017@hotmail.com>
2023-12-16 22:20:05 -0300

    add: attributes and getters and setters on Veiculo class

--- Veiculo.java
+++ Veiculo.java
@@ -2,9 +2,27 @@ package First_POO_Project;
 
 public class Veiculo extends Caracteristicas implements CalcConsumo{
 
+    private String modelo;
+    private String marca;
 
     @Override
     public float consumo(float media, int kilometragem){
         return media * kilometragem;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
     }
 }
