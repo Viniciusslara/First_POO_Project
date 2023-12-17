6d8dbd5e641453c3ff41944ebe40f8692967704b
Viniciusslara <vinicius.lara2017@hotmail.com>
2023-12-16 21:55:50 -0300

    add: caracteristicas class attributes

---
+++ Caracteristicas.java
@@ -0,0 +1,44 @@
+package First_POO_Project;
+
+public class Caracteristicas {
+    
+    private String modelo;
+    private String marca;
+    private String ano;
+    private float consumoM;
+    private String tipoCombustivel;
+    
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getAno() {
+        return ano;
+    }
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+    public float getConsumoM() {
+        return consumoM;
+    }
+    public void setConsumoM(float consumoM) {
+        this.consumoM = consumoM;
+    }
+    public String getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+    public void setTipoCombustivel(String tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    
+
+}
